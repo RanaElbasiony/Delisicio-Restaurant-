41d0d97d6cf112eead7d16df46c3099f8bc5dcb6
Rana Elbasiony <143304331+RanaElbasiony@users.noreply.github.com>
2023-10-20 01:35:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var btn= document.querySelector(".Reserve .btn");
+btn.onclick=function(){
+    alert("Done Reservation");
+}
+// to do reserve btn
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();   
+});
